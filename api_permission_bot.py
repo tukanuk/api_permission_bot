ac6d0d0d8c5e33a370705006e837bbe6b744e763
Tukanuk <benjamin@idavidson.ca>
2020-05-05 16:31:20 -0400

    working version

--- api_permission_bot.py
+++ api_permission_bot.py
@@ -3,7 +3,11 @@ import requests
 from requests.exceptions import HTTPError
 import simplelogging
 import json
+import pandas
 
+
+# TODO: to remove the URL column and make API column linkable I'd have to build the indivdiual tables by hand. 
+# TODO: or maybe use soup to edit every place a <tr><td><ADD LINK HERE>name</a> occurs
 USE_LOCAL_OBJECTS = True
 
 log = simplelogging.get_logger(
@@ -11,6 +15,8 @@ log = simplelogging.get_logger(
 
 
 def main():
+
+    # Get the inital page (used for scraping the menu)
     if USE_LOCAL_OBJECTS:
         with open('dynatrace_soup.html', 'r') as fp:
             dynatrace_soup = BeautifulSoup(fp.read(), features="html.parser")
@@ -19,6 +25,7 @@ def main():
     else:
         dynatrace_soup = get_api_start_doc()
 
+    # Scrape the menu
     menu_items_dict = get_menu_items(dynatrace_soup)
 
     # test the menu_items_dict
@@ -32,12 +39,85 @@ def main():
         log.info("Dictionary tests passsed")
         log.debug("menu_item_dict[\'GET all rules\'] == expected URL")
 
-    api_authentication_info = api_doc_crawler(menu_items_dict)
+    # Scrape the actual pages from the menu items
+    if USE_LOCAL_OBJECTS:
+        with open('api_info.json', 'r') as fp:
+            api_authentication_info = json.load(fp)
+    else:
+        api_authentication_info = api_doc_crawler(menu_items_dict)
 
-    # TODO: build pandas
-    # TODO: group by
-    # TODO: build individual html files
-    # TODO: combine into one static file
+    # Build the df
+    all_the_info = data_manipulation(api_authentication_info)
+
+    log.debug(f"Name: {all_the_info[0][0]}")
+    log.debug(f"List: {all_the_info[0][1]}")
+    log.debug(f"HTML: {all_the_info[0][2]}")
+
+    build_the_html(all_the_info)
+
+
+def build_the_html(all_the_info):
+    # Build the final output HTML document
+
+    head = """<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>API Permissions</title>
+  </head>"""
+    body = "<body><div class=\"container\"><h1>API Permissions</h1>"
+
+    # add anchor links
+    body += "<ul>"
+    for item in all_the_info:
+        body += "<li><a href=\"#" + item[0] + "\">" + item[0] + "</a></li>"
+    body += "</ul>"
+
+    # add the tables
+    for item in all_the_info:
+        body += "<h2><a id=\"" + item[0] + "\"></a>" + item[0] + "</h2>"
+        body += item[2]
+
+    body += "</div></body>"
+    footer = "</html>"
+
+    with open('api_permission.html', 'w') as fp:
+
+        fp.write(head)
+        fp.write(body)
+        fp.write(footer)
+
+
+def data_manipulation(api_authentication_info):
+    # Group by permission
+    # Returns a list of API calls grouped by permisison
+    df = pandas.DataFrame(api_authentication_info, columns=[
+                          'API', 'Permission', 'Section', 'URL'])
+    df_groups = df.groupby("Permission")
+    groupNames = df_groups.groups
+    sections = []
+    for name in groupNames:
+        # Add to list
+        log.debug(f"Adding {name}")
+        group = df_groups.get_group(name)
+        log.debug(f"Type of group: {type(group)}")
+
+        # save to html
+        with open("html/" + name + ".html", 'w') as fp:
+            group.sort_values(by=['Section'])[['API', 'Section', 'URL']].to_html(
+                fp, index=False, border=None, render_links=True, classes='table table-hover')
+
+        sections.append(
+            [name, group[['API', 'Section', 'URL']], group.sort_values(by=['Section'])[['API', 'Section', 'URL']].to_html(
+                index=False, border=0, render_links=True, classes='table table-hover table-sm')])
+
+    return sections
 
 
 def get_api_start_doc():
