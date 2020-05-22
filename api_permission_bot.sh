0b6fead666d11163afc858c2bd9c4108fcf35304
EC2 Default User <ec2-user@ip-172-31-36-109.us-east-2.compute.internal>
2020-05-22 19:38:06 +0000

    added fixe paths

--- api_permission_bot.sh
+++ api_permission_bot.sh
@@ -2,5 +2,5 @@
 # Setup up the venv, runs the script, copies file to /var/www/html
 
 source ~/api_permission_bot/venv/bin/activate
-python3 api_permission_bot.py
-cp api_permission.html /var/www/html/index.html
+python3 ~/api_permission_bot/api_permission_bot.py
+cp ~/api_permission_bot/api_permission.html /var/www/html/index.html
